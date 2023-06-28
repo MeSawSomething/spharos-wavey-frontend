60ddfe95eb6c139af8d32cb10825f30ee723a161
MeSawSomething <miso.hong.1989@gmail.com>
2023-06-28 19:09:48 +0900

    Refactor: reverse the PaymentReady page

--- components/pages/car/PaymentReady.tsx
+++ components/pages/car/PaymentReady.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRecoilValue } from "recoil";
 import PageLoader from "@/components/ui/PageLoader";
 import { authState } from "@/state/authState";
@@ -37,38 +37,31 @@ export default function PaymentReady(props: {
     reward: 1000,
   };
 
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
   useEffect(() => {
     const getPaymentReady = async () => {
-      try {
-        const res = await axios.post(
-          `${API_URL}/purchase/kakao/ready`,
-          readyRequestBody,
-          {
-            headers: {
-              Authorization: TOKEN,
-            },
-          }
-        );
-        sessionStorage.setItem("purchaseNumber", res.data.purchaseNumber);
-        router.push(res.data.next_redirect_mobile_url);
-      } catch (error) {
-        router.push("/purchase/fail");
-      } finally {
-        setIsLoading(false);
-      }
+      const res = await axios.post(
+        `${API_URL}/purchase/kakao/ready`,
+        readyRequestBody,
+        {
+          headers: {
+            Authorization: TOKEN,
+          },
+        }
+      );
+      sessionStorage.setItem("purchaseNumber", res.data.purchaseNumber);
+      router.push(res.data.next_redirect_mobile_url);
     };
     getPaymentReady();
-  }, []);
+  }, [router, readyRequestBody, TOKEN, API_URL]);
 
-  if (isLoading) {
-    return (
-      <div className={style.loaderContainer}>
+  return (
+    <>
+      <div
+        className={style.over}
+        style={props.isOpen ? { display: "block" } : { display: "none" }}
+      >
         <PageLoader text="결제 페이지로 이동합니다." />
       </div>
-    );
-  }
-
-  return <></>;
+    </>
+  );
 }
